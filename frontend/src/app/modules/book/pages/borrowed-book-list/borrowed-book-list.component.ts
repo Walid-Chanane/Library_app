2709f3ba89d95770a4eeaa441cede49d24343e5b
Walid-Chanane <walid.chanane01@gmail.com>
2024-08-15 15:45:53 +0100

    add return borrowed book and feedback features

--- frontend/src/app/modules/book/pages/borrowed-book-list/borrowed-book-list.component.ts
+++ frontend/src/app/modules/book/pages/borrowed-book-list/borrowed-book-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BorrowedBookResponse, PageResponseBorrowedBookResponse } from '../../../../services/models';
-import { BookService } from '../../../../services/services';
+import { BorrowedBookResponse, FeedbackRequest, PageResponseBorrowedBookResponse } from '../../../../services/models';
+import { BookService, FeedbackService } from '../../../../services/services';
 
 @Component({
   selector: 'app-borrowed-book-list',
@@ -12,10 +12,16 @@ export class BorrowedBookListComponent implements OnInit{
   borrowedBooks: PageResponseBorrowedBookResponse = {}
   page: number = 0
   size: number = 5
-  selectedBook: BorrowedBookResponse = {}
+  selectedBook: BorrowedBookResponse | undefined = undefined
+  feedbackRequest: FeedbackRequest = {
+    bookId: 0,
+    comment: '',
+    evaluation: 0
+  }
   
   constructor(
-    private bookService: BookService
+    private bookService: BookService,
+    private feedbackService: FeedbackService
   ){}
 
   ngOnInit(): void {
@@ -31,6 +37,7 @@ export class BorrowedBookListComponent implements OnInit{
   
   returnBorrowedBook(book: BorrowedBookResponse) {
     this.selectedBook = book
+    this.feedbackRequest.bookId = book.id as number
   }
   
   goToFirstPage(){
@@ -62,4 +69,25 @@ export class BorrowedBookListComponent implements OnInit{
     return this.page == this.borrowedBooks.totalPages as number -1
   }
 
+  returnBook(withFeedback: boolean){
+    this.bookService.returnBorrowedBook({
+      'bookId': this.selectedBook?.id as number
+    }).subscribe({
+      next: () => {
+        if(withFeedback){
+          this.sendFeedback()
+        }
+        this.selectedBook = undefined
+        this.findAllBorrowedBooks()
+      }
+    })
+  }
+
+  private sendFeedback(){
+    this.feedbackService.saveFeedback({
+      body: this.feedbackRequest
+    }).subscribe({
+      next: () => {}
+    })
+  }
 }
