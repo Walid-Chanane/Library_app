ce7da5e0524c3bcbceb5c2ce88c7863ed520363f
Walid-Chanane <walid.chanane01@gmail.com>
2024-08-20 17:51:27 +0100

    Implement the returned books page

--- frontend/src/app/modules/book/book-routing.module.ts
+++ frontend/src/app/modules/book/book-routing.module.ts
@@ -5,6 +5,7 @@ import { BookListComponent } from './pages/book-list/book-list.component';
 import { MyBooksComponent } from './pages/my-books/my-books.component';
 import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
+import { ReturnedBooksComponent } from './pages/returned-books/returned-books.component';
 
 const routes: Routes = [
   {
@@ -30,6 +31,10 @@ const routes: Routes = [
       {
         path: 'my_borrowed_books',
         component: BorrowedBookListComponent
+      },
+      {
+        path: 'my_returned_books',
+        component: ReturnedBooksComponent
       }
     ]
   }
